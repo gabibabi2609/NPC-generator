44163800363025c5b94a2df96dd4ff8e86b85235
Gabi <monteirogabby01@gmail.com>
2025-10-28 18:23:47 +0000

    occupations

--- NPC_generate.py
+++ NPC_generate.py
@@ -1,9 +1,11 @@
-npc_names = input("Name ten random names: ")
+npc_names = input("\nName ten random names: ")
+
+name_list = npc_names.split(", ")
+print(name_list)
 
 npc_list = []
 npc_list.append(npc_names)
 
-# print(npc_list)
 
 attributes =  [
     "bloodstained",
@@ -17,36 +19,22 @@ attributes =  [
     "haunting",
     "ashen",
     "glowing",
-    "stitched",
-    "banshee",
-    "possessed",
-    "soulless",
-    "skeletal",
-    "mutated",
-    "phantom",
-    "tattered",
-    "gory",
-    "grim",
-    "lurking",
-    "eldritch",
-    "hollow",
-    "ominous",
-    "distorted",
-    "ravenous",
-    "bleeding",
-    "burned",
-    "pale",
-    "sinister",
-    "crimson",
-    "silent",
-    "vile",
-    "cold",
-    "deranged",
-    "tormented",
-    "infected",
-    "unholy",
-    "macabre"
+    "evil"
 ]
 
-import random 
-print(random.choice(npc_names), random.choice(attributes), random.choice(attributes), random.choice(attributes), random.choice(attributes))
+import random
+import time
+
+for name in range(len(name_list)):
+    print(f"\nCharacter: {random.choice(name_list)}")
+    print(f"Attributes: {random.choice(attributes)}, {random.choice(attributes)}, {random.choice(attributes)}")
+
+
+occupations = ["blacksmith", "royal", "knight", "jester", "farmer", "cook"]
+
+for age in range(len(name_list)):
+    age_of_character = int(input(f"\nHow old is {random.choice(name_list)}? "))
+    if age_of_character >= 7:
+        print(f"\nTheir occupation is: {random.choice(occupations)}\n")
+    else:
+        print("\nThey are too young to work\n")
