a37ac99a41807663a40437221b2b55e6f684997c
Gabi <monteirogabby01@gmail.com>
2025-11-02 16:32:07 +0000

    finished

--- NPC_generate.py
+++ NPC_generate.py
@@ -11,18 +11,21 @@ import random
 import time
 
     
-occupations = ["blacksmith", "royal", "knight", "jester", "farmer", "cook"]
-
+occupations = ["blacksmith", "knight", "jester", "farmer", "cook", "brewer", "weaver", "miller", "fisher", "carpenter", "priest"]
+status = ["clergy", "nobility", "lords", "peasantry"]
 
 for name in range(len(names_list)):
-    age_of_character = int(input(f"\nHow old is {random.choice(names_list)}? "))
-    shilling_int = random.randint(100, 600)
-    shilling = shilling_int / 100.0
+    shilling_int = random.randint(99, 999)
+    shilling = shilling_int / 10.0
+    npc_trait = random.sample(attributes, 3)
+    trait1 = npc_trait[0]
+    trait2 = npc_trait[1]
+    trait3 = npc_trait[2]
     is_able_fight = random.choice([True, False])
     if is_able_fight == True:
         fight_status ="Yes"
     elif is_able_fight == False:
         fight_status = "No"
-
-print(f"Character: {random.sample(names_list)}\nOccupation: {random.choice(occupations)}\nCan they fight? {fight_status}")
-print("----------------------------------")
+    print(f"\nCharacter: {random.sample(names_list, k = 1)}\nShillings: {shilling}\nOccupation: {random.choice(occupations)}\nCan they fight? {fight_status}\nTraits: {trait1}, {trait2}, and {trait3}\nSocietal status: {random.choice(status)}")
+    print("-----------------------------------------")
+    time.sleep(1.0)
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # NPC-generator
 End of marking period python project
+# 📄 How to write a good README
+
+
+## 🌟 Highlights
+
+
+Here are the main highlights of this code:
+
+- You can input your own character's names
+- Customization of characters
+- Random medieval jobs
+- Not everything is randomized
+
+
+## ℹ️ Overview
+
+The purpose of this project is to make a code that randomly generates 10 distinct non-playable characters (npcs). The user can input their own choices or randomly pick them. Each character is distinct and unique with randomized character attributes. Some characters may not qualify for certain attributes, depending on the user's input. Each character has a name, a fight status, an occupation, shillings, and a societal status. This program uses basic progamming structures.
+
+
+### 🚀 Usage
+
+Step 1: make sure python is installed
+Step 2: open the file name "NPC_generate.py"
+Step 3: Enter at least 10 names into the terminal
+Step 4: Watch as your characters generate
+Step 5: Run as many times as needed
+
+Example output: 
+Character: ['Sarah']
+Shillings: 16.9
+Occupation: Weaver
+Can they fight? No
+Traits: gentle, proud, and prideful
+Societal status: Peasantry
+
+
+### ✍️ Author
+
+Gabriela Monteiro 
+    - CSAEA student
+    - APCSP student
+
